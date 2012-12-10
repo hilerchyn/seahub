9aa4f86cb5626cc125f545c2076e2dbb471c7b6b
zhengxie <xiez1989@gmail.com>
2012-12-10 11:48:19 +0800

    Added repos and repo view

--- api2/urls.py
+++ api2/urls.py
@@ -9,4 +9,8 @@ urlpatterns = patterns('',
     url(r'^ping/$', Ping.as_view()),
     url(r'^auth/ping/$', AuthPing.as_view()),
     url(r'^auth-token/', ObtainAuthToken.as_view()),
+
+    url(r'^repos/$', Repos.as_view()),
+    url(r'^repos/(?P<repo_id>[-0-9a-f]{36})/$', Repo.as_view()),
+
 )
--- api2/views.py
+++ api2/views.py
@@ -10,6 +10,10 @@ from models import Token
 from authentication import TokenAuthentication
 from serializers import AuthTokenSerializer
 from base.accounts import User
+from seahub.views import access_to_repo, validate_owner
+
+from seaserv import seafserv_threaded_rpc, get_personal_groups_by_user, \
+    get_group_repos, get_repo, check_permission, get_commits
 
 class Ping(APIView):
     """
@@ -51,4 +55,154 @@ class ObtainAuthToken(APIView):
             return Response({'token': token.key})
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def calculate_repo_info(repo_list, username):
+    """
+    Get some info for repo.
+
+    """
+    for repo in repo_list:
+        try:
+            commit = get_commits(repo.id, 0, 1)[0]
+            repo.latest_modify = commit.ctime
+            repo.root = commit.root_id
+            repo.size = seafserv_threaded_rpc.server_repo_size(repo.id)
+            if not repo.size :
+                repo.size = 0;
+
+            password_need = False
+            if repo.encrypted:
+                try:
+                    ret = seafserv_rpc.is_passwd_set(repo.id, username)
+                    if ret != 1:
+                        password_need = True
+                except SearpcErroe, e:
+                    pass
+            repo.password_need = password_need
+        except Exception,e:
+            repo.latest_modify = 0
+            repo.commit = None
+            repo.size = -1
+            repo.password_need = None
     
+class Repos(APIView):
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, format=None):
+        email = request.user.username
+
+        owned_repos = seafserv_threaded_rpc.list_owned_repos(email)
+        calculate_repo_info (owned_repos, email)
+        owned_repos.sort(lambda x, y: cmp(y.latest_modify, x.latest_modify))
+
+        n_repos = seafserv_threaded_rpc.list_share_repos(email,
+                                                         'to_email', -1, -1)
+        calculate_repo_info (n_repos, email)
+        owned_repos.sort(lambda x, y: cmp(y.latest_modify, x.latest_modify))
+
+        repos_json = []
+        for r in owned_repos:
+            repo = {
+                "type":"repo",
+                "id":r.id,
+                "owner":email,
+                "name":r.name,
+                "desc":r.desc,
+                "mtime":r.latest_modify,
+                "root":r.root,
+                "size":r.size,
+                "encrypted":r.encrypted,
+                "password_need":r.password_need,
+                }
+            repos_json.append(repo)
+
+        for r in n_repos:
+            repo = {
+                "type":"srepo",
+                "id":r.id,
+                "owner":r.shared_email,
+                "name":r.name,
+                "desc":r.desc,
+                "mtime":r.latest_modify,
+                "root":r.root,
+                "size":r.size,
+                "encrypted":r.encrypted,
+                "password_need":r.password_need,
+                }
+            repos_json.append(repo)
+
+        groups = get_personal_groups_by_user(email)
+        for group in groups:
+            g_repos = get_group_repos(group.id, email)
+            calculate_repo_info (g_repos, email)
+            owned_repos.sort(lambda x, y: cmp(y.latest_modify, x.latest_modify))
+            for r in g_repos:
+                repo = {
+                    "type":"grepo",
+                    "id":r.id,
+                    "owner":group.group_name,
+                    "name":r.name,
+                    "desc":r.desc,
+                    "mtime":r.latest_modify,
+                    "root":r.root,
+                    "size":r.size,
+                    "encrypted":r.encrypted,
+                    "password_need":r.password_need,
+                    }
+                repos_json.append(repo)
+
+        return Response(repos_json)
+
+def can_access_repo(request, repo_id):
+    if not check_permission(repo_id, request.user.username):
+        return False
+    return True
+    
+class Repo(APIView):
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, repo_id, format=None):
+        # check whether user can view repo
+        repo = get_repo(repo_id)
+        if not repo:
+            # return api_error(request, '404')
+            return Response(status.HTTP_404_NOT_FOUND)
+
+        if not can_access_repo(request, repo.id):
+            # return api_error(request, '403')
+            return Response(status.HTTP_403_FORBIDDEN)
+
+        # check whether use is repo owner
+        if validate_owner(request, repo_id):
+            owner = "self"
+        else:
+            owner = "share"
+
+        try:
+            repo.latest_modify = get_commits(repo.id, 0, 1)[0].ctime
+        except:
+            repo.latest_modify = None
+
+        # query repo infomation
+        repo.size = seafserv_threaded_rpc.server_repo_size(repo_id)
+        current_commit = get_commits(repo_id, 0, 1)[0]
+        repo_json = {
+            "type":"repo",
+            "id":repo.id,
+            "owner":owner,
+            "name":repo.name,
+            "desc":repo.desc,
+            "mtime":repo.lastest_modify,
+            "size":repo.size,
+            "encrypted":repo.encrypted,
+            "root":current_commit.root_id,
+            "password_need":repo.password_need,
+            }
+
+        return Response(repo_json)
+
+    def post(self, request, repo_id, format=None):
+        # TODO
+        pass
